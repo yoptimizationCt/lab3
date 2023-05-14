1290ec0e7e102556b0949706cbd5aa70f0eb5ba3
Artem <334947@niuitmo.ru>
2023-05-14 17:59:06 +0300

    Comments added

--- LBFGS.py
+++ LBFGS.py
@@ -1,46 +1,31 @@
 import collections
-
 import calc
 import numpy as np
 
 
-def transpose(vector):
-    d = len(vector)
-    ret = np.zeros((d, 1))
-    for i in range(d):
-        ret[i][0] = vector[i]
-    return np.array(ret)
-
-
-def get_v(s, y, ro, dim):
-    return np.identity(dim) - ro * np.outer(s, y)
-
-
-def get_u(ro, s):
-    return ro * np.outer(s, s)
-
-
-def update(s, y, matrix_h, ro, dim):
-    matrix_v = get_v(s=s, y=y, ro=ro, dim=dim)
-    matrix_v_t = np.transpose(matrix_v)
-    matrix_u = get_u(ro=ro, s=s)
-    return np.dot(np.dot(matrix_v_t, matrix_h), matrix_v) + matrix_u
-
-
 def l_bfgs(f, start_point, epoch=10000,  m=10):
+    # инициируем некоторые константы и начальные значения
     grad_eps = 1e-7
     x = start_point
-    g = calc.gradient(f, x)
     dim = len(start_point)
+    g = calc.gradient(f, x)
     points = np.zeros((epoch + 1, dim))
     points[0] = start_point
+
+    alpha = np.zeros(m)
+    beta = np.zeros(m)
+
+    # заводим дэк для хранения не более m изменений значений
     last_updates_s = collections.deque()
     last_updates_y = collections.deque()
     last_updates_ro = collections.deque()
-    alpha = np.zeros(m)
-    beta = np.zeros(m)
+
+    # инициируем стартовое смещение
     z = 0.018 * g
+
     for k in range(1, epoch+1):
+
+        # получаем новое значение х и высчитываем на delta_x и delta_gradient
         x = x - z
         points[k] = x
         g_1 = calc.gradient(f, x)
@@ -48,17 +33,23 @@ def l_bfgs(f, start_point, epoch=10000,  m=10):
         y = g_1 - g
         ro = 1 / (np.dot(y, s))
         g = g_1
+
+        # если норма градиента меньше эпсилон, то стоп
         if np.linalg.norm(g) < grad_eps:
             print("Потребовалось итераций " + str(k))
             break
-
         if k > m:
+            # в дэке должно быть не больше m элементов
             last_updates_s.popleft()
             last_updates_y.popleft()
             last_updates_ro.popleft()
+
+        # добавляем элементы в дэк
         last_updates_s.append(s)
         last_updates_y.append(y)
         last_updates_ro.append(ro)
+
+        # вычисляем новое смещение
         q = g
         size = len(last_updates_s)
         for i in range(size - 1, -1, -1):
@@ -73,3 +64,4 @@ def l_bfgs(f, start_point, epoch=10000,  m=10):
             z = z + last_updates_s[i] * (alpha[i] - beta[i])
 
     return x
+
