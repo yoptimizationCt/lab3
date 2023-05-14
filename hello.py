6e2c986824aa425e00ef1e6420bc5750df00e751
Artem <334947@niuitmo.ru>
2023-05-14 17:44:42 +0300

    BFGS done

--- hello.py
+++
@@ -1,8 +0,0 @@
-class Hello:
-    message = "Hello, "
-
-    def __init__(self, name):
-        self.message += name
-
-    def say_hello(self):
-        print(self.message)
--- main_Elfinator.py
+++
@@ -1,5 +0,0 @@
-import hello
-
-name = input()
-myHello = hello.Hello(name)
-myHello.say_hello()
